6a2bf145851322801103728c34b8a3fefe0aea27
wasieassefa65 <102624449+wasieassefa65@users.noreply.github.com>
2022-03-31 09:50:06 -0500

    Update ds.R

--- R/ds.R
+++ R/ds.R
@@ -11,17 +11,16 @@ ds<-function(x){
 
   # 1 row and 2 colmns
   par(mfrow=c(1,2))
-
 # histogram
   hist(x,col=("red"))
  # boxpot
   boxplot(x,col=("green"))
    par(mfrow=col(1,1))
   ## numeric summary 
-  
   data.frame(mean=mean(x),
             min=min(x),
             max=max(x),
             sd=sd(x),
-        vaince= variance(x))
+          range=max(x)-min(x),
+         median=median(x))
 }
